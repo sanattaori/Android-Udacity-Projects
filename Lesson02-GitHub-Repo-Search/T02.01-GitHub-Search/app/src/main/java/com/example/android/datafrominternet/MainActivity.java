be9686cbc4a6e0dcc3cdee1f1588a7888472cf46
sanattaori <sanattaori3@gmail.com>
2018-02-16 21:20:27 +0530

    Async task completed & added data to json

--- Lesson02-GitHub-Repo-Search/T02.01-GitHub-Search/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.01-GitHub-Search/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,12 +15,16 @@
  */
 package com.example.android.datafrominternet;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,10 +39,12 @@ public class MainActivity extends AppCompatActivity {
     EditText mSearchBoxet;
     TextView mUrlDisplayTv;
     TextView mSearchResultTv;
+    TextView erroMsgTv;
+
     // TODO DONE(27) Create a TextView variable called mUrlDisplayTextView
     // TODO DONE(28) Create a TextView variable called mSearchResultsTextView
 
-
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +55,22 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxet =  findViewById(R.id.edit_text_search_github);
         mUrlDisplayTv = findViewById(R.id.tv_url_display);
         mSearchResultTv = findViewById(R.id.tv_search_results);
+        erroMsgTv = findViewById(R.id.tv_error_message_display);
         // TODO DONE(30) Use findViewById to get a reference to mUrlDisplayTextView
         // TODO DONE(31) Use findViewById to get a reference to mSearchResultsTextView
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
     }
-    //
+    //show error
+    private void showErrorMessage() {
+        mSearchResultTv.setVisibility(View.INVISIBLE);
+        erroMsgTv.setVisibility(View.VISIBLE);
+    }
+
+    private void showJsonDataView() {
+        erroMsgTv.setVisibility(View.INVISIBLE);
+        mSearchResultTv.setVisibility(View.VISIBLE);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
@@ -69,20 +87,55 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    @SuppressLint("StaticFieldLeak")
+    public class GithubAsyncTaskQuery extends AsyncTask<URL, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                mSearchResultTv.setText(githubSearchResults);
+            }
+
+            else {
+                showErrorMessage();
+            }
+        }
+    }
+
     private void makeGithubSearchQuery() {
         //get text from search box
         String githubQuery = mSearchBoxet.getText().toString();
         //pass to NetworkUtils BuildUrl
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         //set result to display TextView
-        mSearchResultTv.setText(String.format("Requesting url : %s", githubSearchUrl.toString()));
+        mSearchResultTv.setText(String.format("Wait Searching/Requesting url : %s", githubSearchUrl.toString()));
         try {
-            String GitHubSearchResults = NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
-            mSearchResultTv.setText(GitHubSearchResults);
-        } catch (IOException e) {
+            new GithubAsyncTaskQuery().execute(githubSearchUrl);
+            mSearchResultTv.setText(githubSearchUrl.toString());
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
 
 }
