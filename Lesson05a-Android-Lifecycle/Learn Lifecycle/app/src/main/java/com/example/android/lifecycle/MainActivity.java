2b7db18a9516a1e36f73b7754fedc4dc02663abc
sanattaori <sanattaori3@gmail.com>
2018-02-23 16:10:48 +0530

    activity lifecycle on rotate device

--- Lesson05a-Android-Lifecycle/Learn Lifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/Learn Lifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -48,19 +48,63 @@ public class MainActivity extends AppCompatActivity {
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
         // TODO (1) Use logAndAppend within onCreate
+        logAndAppend("inside oncreate");
     }
 
     // TODO (2) Override onStart, call super.onStart, and call logAndAppend with ON_START
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        logAndAppend("inside onStart");
+    }
+
     // TODO (3) Override onResume, call super.onResume, and call logAndAppend with ON_RESUME
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        logAndAppend("onResume");
+    }
+
+
 
     // TODO (4) Override onPause, call super.onPause, and call logAndAppend with ON_PAUSE
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        logAndAppend("onPause");
+        Log.d(TAG, "onPause: inside onPause");
+    }
+
+
     // TODO (5) Override onStop, call super.onStop, and call logAndAppend with ON_STOP
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        logAndAppend("onStop");
+    }
+
 
     // TODO (6) Override onRestart, call super.onRestart, and call logAndAppend with ON_RESTART
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        logAndAppend("onRestart");
+    }
+
+
     // TODO (7) Override onDestroy, call super.onDestroy, and call logAndAppend with ON_DESTROY
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logAndAppend("onDestory");
+    }
 
     /**
      * Logs to the console and appends the lifecycle method name to the TextView so that you can
